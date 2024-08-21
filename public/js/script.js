165ca4132ba6d169e8fbb09bcbaf02a56111fce8
Mohamed H. Kamal <mohamedhanykamal214@gmail.com>
2024-08-20 21:42:54 -0500

    Edited 2025

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,23 @@
 
-// const countDownDate = new Date("Feb 11, 2024 12:00:00").getTime();
+const countDownDate = new Date("Feb 11, 2024 12:00:00").getTime();
 
-// const x = setInterval(function() {
-//     const now = new Date().getTime();
-//     const distance = countDownDate - now;
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
     
-//     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-//     document.getElementById("days").innerHTML = days;
-//     document.getElementById("hours").innerHTML = hours;
-//     document.getElementById("minutes").innerHTML = minutes;
-//     document.getElementById("seconds").innerHTML = seconds;
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
     
-//     if (distance < 0) {
-//         clearInterval(x);
-//         document.querySelector(".countdown-text").innerHTML = "EVENT STARTED!";
-//     }
-// }, 1000);
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown-text").innerHTML = "EVENT STARTED!";
+    }
+}, 1000);
 
