b782a13c6cfd00305f31781c1e5aa877458008d3
Mohamed Abdelnaby <mohamedhanykamal214@gmail.com>
2025-01-02 23:27:49 -0600

    edit

--- src/components/SponsorPage.js
+++ src/components/SponsorPage.js
@@ -8,28 +8,29 @@ import Sponsor1 from '../assets/SS_Sponsor1.png';
 import Sponsor2 from '../assets/SS_Sponsor2_Google.png';
 
 const SponsorPage = () => {
-
     return (
         <div className="sponsor-page">
 
             {/* Desktop Layout */}
             <div className="sponsor-page-desktop">
-
                 <img className="SpecialThanks" src={SpecialThanks} alt="SpecialThanks" />
                 <img className="MadeWithLove" src={MadeWithLove} alt="MadeWithLoveInNorman" />
 
                 <img className="Sponsor1" src={Sponsor1} alt="Sponsor1 (Home Creations)" />
                 <img className="Sponsor2" src={Sponsor2} alt="Sponsor2 (Google)" />
-
             </div>
 
             {/* Mobile Layout */}
             <div className="sponsor-page-mobile">
+                <img className="SpecialThanks" src={SpecialThanks} alt="SpecialThanks" />
+                <img className="MadeWithLove" src={MadeWithLove} alt="MadeWithLoveInNorman" />
 
+                <img className="Sponsor1" src={Sponsor1} alt="Sponsor1 (Home Creations)" />
+                <img className="Sponsor2" src={Sponsor2} alt="Sponsor2 (Google)" />
             </div>
 
         </div>
-      );
+    );
 };
 
 export default SponsorPage;
