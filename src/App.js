ed32d1e679c6a2a5ae755444de5c06175dea9ef8
Mohamed Abdelnaby <mohamedhanykamal214@gmail.com>
2025-02-05 09:31:21 -0600

    Added Page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// src/App.js
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import LandingPage from './components/LandingPage';
@@ -6,16 +8,25 @@ import RatBreak from './components/RatBreak';
 import AboutPage from './components/AboutPage';
 import FaqPage from './components/FaqPage';
 import SponsorPage from './components/SponsorPage';
+import BuildOnEthereum from './components/BuildOnEthereum'; 
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-      <RatBreak />
-      <AboutPage />
-      <FaqPage />
-      <SponsorPage />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="App">
+            <LandingPage />
+            <RatBreak />
+            <AboutPage />
+            <FaqPage />
+            <SponsorPage />
+          </div>
+        }
+      />
+      <Route path="/builtonethereum" element={<BuildOnEthereum />} />
+    </Routes>
   );
 }
 
